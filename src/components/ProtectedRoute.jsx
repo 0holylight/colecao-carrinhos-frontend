fd9fb280963631d1907d3386e0d155b06505e8f7
Guilherme <gui.estudo00@gmail.com>
2026-08-20 19:27:33 -0300

    feat: configura Axios com cookie httpOnly e reescreve ProtectedRoute

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,14 +1,31 @@
 import { Outlet, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import api from '../services/api.js';
 
 function ProtectedRoute() {
+  const [check, setCheck] = useState(true);
+  const [auth, setAuth] = useState(false);
 
-  const token = localStorage.getItem('token');
+  useEffect(() => {
+    async function verificarLogin() {
+      try {
+        await api.get('/carros');
+        setAuth(true);
+      } catch (e) {
+        setAuth(false);
+      } finally {
+        setCheck(false);
+      }
+    }
 
-  if (token) {
-    return <Outlet />;
-  } else {
-    return <Navigate to="/login" />;
+    verificarLogin();
+  }, []);
+
+  if (check) {
+    return <p>Carregando...</p>;
   }
+
+  return auth ? <Outlet /> : <Navigate to="/login" />;
 }
 
 export default ProtectedRoute;
