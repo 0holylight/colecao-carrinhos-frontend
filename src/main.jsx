cc076d7ff8ad8c8610e47289fd9d0aedfd69e4de
Guilherme <gui.estudo00@gmail.com>
2026-09-10 12:40:15 -0300

    feat: configura Redux com Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+import App from "./App.jsx";
+import store from "./store/store.js";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />    
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
-
   </StrictMode>,
-)
+);
