98c3a2532e08883947285f33ef49312615c784ca
Guilherme <gui.estudo00@gmail.com>
2026-09-25 15:38:36 -0300

    fix: finaliza formulario de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "../store/slices/authSlice.js";
@@ -10,35 +10,40 @@ function Login() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [erro, setErro] = useState("");
 
-  async function handleSubmit() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setErro("");
     try {
       const resposta = await api.post("/tokens", { username, password });
       dispatch(login(resposta.data.user));
       navigate("/carros");
     } catch (e) {
       console.log(e);
+      setErro("Usuário ou senha incorretos.");
     }
   }
 
   return (
     <>
-      <h1>Login</h1>;
-      <form>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
         Login
         <input
           type="text"
           placeholder="collector123"
           value={username}
-          onChange={setUsername}
+          onChange={(e) => setUsername(e.target.value)}
         ></input>
         <input
           type="password"
           placeholder="*******"
           value={password}
-          onChange={setPassword}
+          onChange={(e) => setPassword(e.target.value)}
         ></input>
         <button type="submit">Entrar</button>
+        {erro && <p>{erro}</p>}
       </form>
     </>
   );
